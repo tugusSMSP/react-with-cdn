31207345865cec22077f649e543a8b6d7ea86f7d
tugusSMSP <tugus@smsperkasa.com>
2023-07-18 08:32:00 +0800

    create reacct with cdn

---
+++ index.js
@@ -0,0 +1,9 @@
+class App extends React.Component{
+    render(){
+        return(
+            <h1>Ini project pertamaku tanpa CRA</h1>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
